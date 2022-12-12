32fd2d5e44000b99f3f089464ed4e69473f1a807
Sagar <93700292+syntacticsagar@users.noreply.github.com>
2022-12-12 21:52:53 +0000

    taking zls for a spin

--- 2022/day_01/main.zig
+++ 2022/day_01/main.zig
@@ -4,7 +4,6 @@ const std = @import("std");
 const mem = std.mem;
 const parseInt = std.fmt.parseInt;
 
-
 const file_name = "./input/main_input.txt";
 const file_contents = @embedFile(file_name);
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
@@ -18,31 +17,31 @@ pub fn main() !void {
     var elves_list = std.ArrayList(i64).init(gpa);
     defer elves_list.deinit();
 
-    while(lines.next()) |line| {
-        if(line.len == 0){
-            if(elf_total_calories != 0){
+    while (lines.next()) |line| {
+        if (line.len == 0) {
+            if (elf_total_calories != 0) {
                 try elves_list.append(elf_total_calories);
                 elf_total_calories = 0;
             }
         } else {
             const calories = try parseInt(i64, line, 10);
-            elf_total_calories += calories; 
+            elf_total_calories += calories;
         }
     }
 
     var max_cals: [3]i64 = .{ 0, 0, 0 };
-    for(elves_list.items) |curr_elf_calories| {
-        if(curr_elf_calories > max_cals[0]){
+    for (elves_list.items) |curr_elf_calories| {
+        if (curr_elf_calories > max_cals[0]) {
             max_cals[2] = max_cals[1];
             max_cals[1] = max_cals[0];
             max_cals[0] = curr_elf_calories;
-        } else if(curr_elf_calories > max_cals[1]) {
+        } else if (curr_elf_calories > max_cals[1]) {
             max_cals[2] = max_cals[1];
             max_cals[1] = curr_elf_calories;
-        } else if(curr_elf_calories > max_cals[2]) {
+        } else if (curr_elf_calories > max_cals[2]) {
             max_cals[2] = curr_elf_calories;
         }
     }
-    std.debug.print("problem 1 solution = {}\n", .{ max_cals[0] });
-    std.debug.print("problem 2 solution = {}\n", .{ max_cals[0] + max_cals[1] + max_cals[2] });
+    std.debug.print("problem 1 solution = {}\n", .{max_cals[0]});
+    std.debug.print("problem 2 solution = {}\n", .{max_cals[0] + max_cals[1] + max_cals[2]});
 }
--- shell.nix
+++ shell.nix
@@ -5,8 +5,27 @@ let
   zig_version = "0.10.0";
   zig_overlay = builtins.fetchTarball sources.zig-overlay.url;
   zig_0_10_0 = (import zig_overlay { pkgs = pkgs; }).${zig_version};
+
+  zig_master = (import zig_overlay { pkgs = pkgs; }).master;
+
+
+  zls_overlay = pkgs.fetchgit { 
+    url = "https://github.com/zigtools/zls";
+    sha256 = "sha256-/sgm7zcOB7BwxwM7y7GI0be2q0aufK+T0kO0HnSEQf4=";   
+    fetchSubmodules = true; 
+    deepClone = true;
+  };
+  zls = (import zls_overlay { pkgs = pkgs; }).overrideAttrs(attrs: prevAttrs: {
+    version = "${zig_version}.dev";
+    nativeBuildInputs = [zig_master];
+    buildPhase = ''
+      mkdir -p $out
+      zig build install -Dcpu=baseline -Drelease-safe=true -Ddata_version=${zig_version} --prefix $out
+    '';
+  });
 in pkgs.mkShell {
   buildInputs = [
     zig_0_10_0
+    zls
   ];
 }
